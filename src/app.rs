3c89727605cb809525d0d8d0c95b4240d1e8ff62
Valchap <valchap616@gmail.com>
2022-12-19 19:03:53 +0100

    Column names and remove column

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,7 @@ use evalexpr::*;
 #[derive(Clone)]
 enum PopupStatus {
     None,
-    ColumnSettings(String),
+    ColumnSettings(usize),
 }
 
 pub struct App {
@@ -14,14 +14,14 @@ pub struct App {
 
 pub struct ColumnSettings {
     name: String,
-    formula: String,
+    expression: String,
 }
 
 impl ColumnSettings {
     pub fn new(name: String) -> Self {
         ColumnSettings {
             name,
-            formula: "".to_owned(),
+            expression: "".to_owned(),
         }
     }
 }
@@ -40,15 +40,19 @@ impl App {
         app
     }
 
-    pub fn get_column_by_name_mut(&mut self, name: &String) -> Option<&mut ColumnSettings> {
-        self.columns.iter_mut().find(|column| column.name == *name)
-    }
-
     pub fn add_column(&mut self, name: String) {
         self.columns.push(ColumnSettings::new(name));
 
         for line in self.grid_values.iter_mut() {
             line.push("".to_owned());
+        }
+    }
+
+    pub fn remove_column(&mut self, index: usize) {
+        self.columns.remove(index);
+
+        for line in self.grid_values.iter_mut() {
+            line.remove(index);
         }
     }
 
@@ -77,7 +81,7 @@ impl App {
     }
 
     pub fn get_value(&self, line: usize, column: usize) -> f64 {
-        if self.columns[column].formula.is_empty() {
+        if self.columns[column].expression.is_empty() {
             self.grid_values[line][column]
                 .parse::<f64>()
                 .unwrap_or(f64::NAN)
@@ -91,7 +95,7 @@ impl App {
             }
 
             let result: Result<f64, EvalexprError> =
-                eval_number_with_context(&self.columns[column].formula, &context);
+                eval_number_with_context(&self.columns[column].expression, &context);
 
             result.unwrap_or(f64::NAN)
         }
@@ -105,39 +109,61 @@ impl eframe::App for App {
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        if let PopupStatus::ColumnSettings(name) = self.popup_status.clone() {
+        ctx.set_visuals(egui::Visuals::dark());
+
+        if let PopupStatus::ColumnSettings(column_index) = self.popup_status.clone() {
             let mut open = true;
-            egui::Window::new(format!("⚙ column {name}"))
+            egui::Window::new("⚙ column settings")
                 .collapsible(false)
                 .open(&mut open)
                 .show(ctx, |ui| {
                     ui.vertical(|ui| {
                         let mut context = HashMapContext::new();
 
-                        for column in self.columns.iter() {
+                        for i in (column_index + 1)..self.columns.len() {
                             context
-                                .set_value(column.name.clone(), f64::NAN.into())
+                                .set_value(self.columns[i].name.clone(), f64::NAN.into())
                                 .unwrap();
                         }
 
-                        if let Some(column) = self.get_column_by_name_mut(&name) {
-                            ui.label("Expression");
+                        ui.label("Name");
 
-                            let result: Result<f64, EvalexprError> =
-                                eval_number_with_context(&column.formula, &context);
+                        let text_edit = egui::widgets::TextEdit::singleline(
+                            &mut self.columns[column_index].name,
+                        );
 
-                            let mut text_edit =
-                                egui::widgets::TextEdit::singleline(&mut column.formula);
+                        let name_input = ui.add(text_edit);
 
-                            if result.is_err() {
-                                text_edit = text_edit.text_color(egui::Color32::RED);
-                            }
+                        if name_input.lost_focus() {
+                            self.columns[column_index].name =
+                                self.columns[column_index].name.trim().to_owned();
+                        }
 
-                            let input = ui.add(text_edit);
+                        ui.label("Expression");
 
-                            if input.lost_focus() {
-                                column.formula = column.formula.trim().to_owned();
-                            }
+                        let result: Result<f64, EvalexprError> = eval_number_with_context(
+                            &self.columns[column_index].expression,
+                            &context,
+                        );
+
+                        let mut text_edit = egui::widgets::TextEdit::singleline(
+                            &mut self.columns[column_index].expression,
+                        );
+
+                        if result.is_err() {
+                            text_edit = text_edit.text_color(egui::Color32::RED);
+                        }
+
+                        let expression_input = ui.add(text_edit);
+
+                        if expression_input.lost_focus() {
+                            self.columns[column_index].expression =
+                                self.columns[column_index].expression.trim().to_owned();
+                        }
+
+                        if column_index > 1 && ui.button("remove column").clicked() {
+                            self.popup_status = PopupStatus::None;
+                            self.remove_column(column_index);
                         }
                     });
                 });
@@ -175,8 +201,8 @@ impl eframe::App for App {
             let mut xy_sum = 0.0;
 
             for line in 0..self.grid_values.len() {
-                let x = self.get_value(line, 0);
-                let y = self.get_value(line, 1);
+                let x = self.get_value(line, 1);
+                let y = self.get_value(line, 0);
 
                 if x.is_nan() || y.is_nan() {
                     continue;
@@ -219,15 +245,16 @@ impl eframe::App for App {
                 .striped(true)
                 .resizable(true)
                 .header(20.0, |mut header| {
-                    header.col(|_ui| {});
+                    header.col(|ui| {
+                        ui.heading("i");
+                    });
 
-                    for column in &self.columns {
+                    for column_index in 0..self.columns.len() {
                         header.col(|ui| {
                             ui.horizontal(|ui| {
-                                ui.heading(column.name.to_owned());
+                                ui.heading(self.columns[column_index].name.to_owned());
                                 if ui.button("⚙".to_owned()).clicked() {
-                                    self.popup_status =
-                                        PopupStatus::ColumnSettings(column.name.clone());
+                                    self.popup_status = PopupStatus::ColumnSettings(column_index);
                                 }
                             });
                         });
@@ -236,6 +263,7 @@ impl eframe::App for App {
                     header.col(|ui| {
                         if ui.button("New Column").clicked() {
                             self.add_column("a".to_owned());
+                            self.popup_status = PopupStatus::ColumnSettings(self.columns.len() - 1)
                         }
                     });
                 })
@@ -250,21 +278,34 @@ impl eframe::App for App {
 
                             for x in 0..self.columns.len() {
                                 row.col(|ui| {
-                                    let valid = !self.get_value(y, x).is_nan();
+                                    if self.columns[x].expression.is_empty() {
+                                        let valid = !self.get_value(y, x).is_nan();
 
-                                    let mut text_edit = egui::widgets::TextEdit::singleline(
-                                        &mut self.grid_values[y][x],
-                                    );
+                                        let mut text_edit = egui::widgets::TextEdit::singleline(
+                                            &mut self.grid_values[y][x],
+                                        );
 
-                                    if !valid {
-                                        text_edit = text_edit.text_color(egui::Color32::RED);
-                                    }
+                                        if !valid {
+                                            text_edit = text_edit.text_color(egui::Color32::RED);
+                                        }
 
-                                    let input = ui.add(text_edit);
+                                        let input = ui.add(text_edit);
 
-                                    if input.lost_focus() {
-                                        self.grid_values[y][x] =
-                                            self.grid_values[y][x].trim().to_owned();
+                                        if input.lost_focus() {
+                                            self.grid_values[y][x] =
+                                                self.grid_values[y][x].trim().to_owned();
+                                        }
+                                    } else if y != self.grid_values.len() - 1 {
+                                        let value = self.get_value(y, x);
+
+                                        let mut rich_text =
+                                            egui::RichText::new(format!("{}", value));
+
+                                        if value.is_nan() {
+                                            rich_text = rich_text.color(egui::Color32::RED);
+                                        }
+
+                                        ui.label(rich_text);
                                     }
                                 });
                             }
